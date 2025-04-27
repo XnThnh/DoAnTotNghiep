e2602f198c2dd1720a54ac293b5ac2ccf5787501
Thanh <nxthanh7d@gmail.com>
2025-04-27 10:58:32 +0700

    Add dependency

--- Project/app/build.gradle.kts
+++ Project/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.quanlynhahang"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.quanlynhahang"
@@ -25,6 +25,9 @@ android {
             )
         }
     }
+    buildFeatures{
+        viewBinding = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -40,4 +43,23 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // ViewModel & LiveData
+    implementation("androidx.lifecycle:lifecycle-viewmodel:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-livedata:2.8.7")
+
+    // Retrofit & Gson
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    // Retrofit Adapter cho RxJava2
+    implementation("com.squareup.retrofit2:adapter:rxjava2:2.9.0") // <-- Sửa thành adapter thay vì adapter-rxjava2 trực tiếp
+
+    // RxJava2 & RxAndroid2
+    implementation("io.reactivex.rxjava2:rxjava:2.2.21")
+    implementation("io.reactivex.rxjava2:rxandroid:2.1.1")
+
+    // Glide (Thư viện tải ảnh)
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+
+    implementation ("com.airbnb.android:lottie:6.3.0")
 }
